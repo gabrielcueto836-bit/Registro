f8d47ca0c1aef8a41cee3089aa1c8906f563515a
Angel Cueto <gabrielcueto836@gmail.com>
2026-03-27 18:45:46 -0500

    first commit

---
+++ middleware/autenticacion_middleware.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+
+const authenticateToken = (req, res, next) => {
+
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if(token == null){
+        return res.status(401).json({error:'Token de autenticacion requerido'});
+        
+        }
+
+        jwt.verify(token, process.env.JWT_SECRET, (err,user) =>{
+
+            if(err){
+                console.error('JWT error de verificacion:', err);
+                return res.status(403).json({error: 'Token invalido o expirado'});
+            }
+
+            req.user = user;
+            next();
+        });
+    };
+
+    module.exports= {authenticateToken}
+    
